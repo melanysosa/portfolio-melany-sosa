96b702402a36a5216ea2026dd007c896f99937b1
melanysosa <sosamelanynoelia@gmail.com>
2022-04-19 23:16:21 -0300

    About changes

--- src/pages/About/useStyles.jsx
+++ src/pages/About/useStyles.jsx
@@ -1,7 +1,7 @@
 const useStyles = () => {
 	return {
 		containerAbout:
-			'w-full h-screen text-gray-300 flex flex-col justify-center items-center  mt-[100px]',
+			'w-full h-full text-gray-300 flex flex-col justify-center items-center  mt-[100px]',
 		divP1: 'max-w-[1000px] w-full grid grid-cols-2 gap-8  pb-8 pl-4 text-center items-center',
 		title: 'text-4xl font-bold inline border-b-4 border-gray-300',
 		divP2: 'max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 ',
